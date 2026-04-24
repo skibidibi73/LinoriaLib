1b506c858cb95c974511766a4ea3335a51c4aeac
bi <w2zb9f8sj7@privaterelay.appleid.com>
2026-04-24 15:09:08 +0900

    Update Thememanager.lua

--- Thememanager.lua
+++ Thememanager.lua
@@ -13,6 +13,8 @@ local ThemeManager = {} do
 		['Tokyo Night'] 	= { 6, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"191925","AccentColor":"6759b3","BackgroundColor":"16161f","OutlineColor":"323232"}') },
 		['Ubuntu'] 			= { 7, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"3e3e3e","AccentColor":"e2581e","BackgroundColor":"323232","OutlineColor":"191919"}') },
 		['Quartz'] 			= { 8, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"232330","AccentColor":"426e87","BackgroundColor":"1d1b26","OutlineColor":"27232f"}') },
+		['neverlose'] 		= { 9, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1a1a1a","AccentColor":"00abff","BackgroundColor":"121212","OutlineColor":"000000"}') },
+		['primordial'] 		= { 10, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"161616","AccentColor":"b77da0","BackgroundColor":"060606","OutlineColor":"2e2e2e"}') },
 	}
 
 	function ThemeManager:ApplyTheme(theme)
